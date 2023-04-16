a6f0ee188fa066d91a493c6663a65ee3a45a39df
Ha Ngoc Diep <109588227+Ha-Diep@users.noreply.github.com>
2023-04-16 10:25:20 +0700

    Add files via upload

---
+++ Analysis.sql
@@ -0,0 +1,288 @@
+﻿USE Zomato
+GO
+
+SELECT TOP 10 * FROM dbo.Zomato_Dataset
+
+
+ALTER TABLE dbo.Zomato_Dataset
+ALTER COLUMN Locality VARCHAR(250)
+
+-- Số nhà hàng trong từng khu vực theo Locality và City của từng quốc gia 
+-- Ví dụ: Ấn độ
+-- Có thể làm thêm về việc viết function cho từng quốc gia
+
+SELECT 
+	--c.Country,
+	d.City,
+	d.Locality,
+	COUNT(d.Locality) AS [Số nhà hàng trong khu vực]
+	,SUM(COUNT(d.Locality)) OVER (
+									PARTITION BY 
+												d.City 
+									ORDER BY 
+												d.Locality DESC
+									) AS [Tổng số nhà hàng trong thành phố] 
+	-- Tính số nhà hàng luỹ kế (không phải là hiện tổng)
+FROM 
+	dbo.Zomato_Dataset d
+INNER JOIN
+	dbo.Country_Name c
+ON 
+	d.CountryCode = c.[Country Code]
+WHERE 
+	c.Country = 'India'
+GROUP BY	
+	--c.Country, 
+	d.City, 
+	d.Locality
+
+
+-- Tính số nhà hàng hiện tổng
+SELECT COUNT(d.RestaurantID) FROM dbo.Zomato_Dataset d
+INNER JOIN
+dbo.Country_Name c
+ON d.CountryCode = c.[Country Code]
+WHERE c.Country = 'India'
+	AND d.City = 'Agra'
+
+GO
+
+
+-- Tính tỉ lệ phần trăm số nhà hàng của từng quốc gia 
+
+WITH ct1 as
+(
+	SELECT
+		c.Country, 
+		COUNT(d.RestaurantID) [Tổng số nhà hàng]
+	FROM 
+		dbo.Zomato_Dataset d
+	INNER JOIN 
+		dbo.Country_Name c
+	ON 
+		d.CountryCode = c.[Country Code]
+	GROUP BY
+		c.Country
+), 
+	ct2 AS
+(
+	SELECT
+		DISTINCT c.Country, 
+		COUNT(d.RestaurantID) OVER() [Tổng số nhà hàng toàn danh sách]
+	FROM 
+		dbo.Zomato_Dataset d
+	INNER JOIN 
+		dbo.Country_Name c
+	ON 
+		d.CountryCode = c.[Country Code]	
+)
+SELECT 
+	ct1.Country, 
+	ct1.[Tổng số nhà hàng],
+	ROUND (CAST(ct1.[Tổng số nhà hàng] AS DECIMAL)
+			/CAST(ct2.[Tổng số nhà hàng toàn danh sách] AS DECIMAL)* 100,2) 
+				AS [Phần trăm]
+FROM 
+	ct1 join ct2 
+ON  ct2.Country = ct1.Country
+ORDER BY 
+	[Phần trăm] DESC
+GO
+
+
+-- Đưa thêm function (hoặc tìm xem quốc gia nào có xếp hạng % thứ x chẳng hạn)
+
+-- Tìm thành phố và khu vực (ở một quốc gia nhất định) với số lượng nhà hàng lớn nhất
+WITH ct1 as
+(
+	SELECT
+		c.Country,
+		d.City,
+		d.Locality,
+		COUNT(d.RestaurantID) AS [Số nhà hàng],
+		DENSE_RANK() OVER(PARTITION BY d.City 
+					 ORDER BY COUNT(d.RestaurantID) DESC) AS [Xếp hạng]
+	FROM
+		dbo.Zomato_Dataset d
+	INNER JOIN 
+		dbo.Country_Name c
+	ON 
+		d.CountryCode = c.[Country Code]
+	WHERE 
+		c.Country = 'India'
+	GROUP BY 
+		c.Country, d.City, d.Locality
+)
+SELECT * FROM ct1 WHERE ct1.[Xếp hạng] <5 AND city = 'New Delhi'
+GO
+
+
+-- Xây dựng function (scalar hoặc table đều được)
+
+-- Tìm thành phố và khu vực (ở một quốc gia nhất định) với số lượng nhà hàng nhỏ nhất 
+WITH ct1 as
+(
+	SELECT
+		c.Country,
+		d.City,
+		d.Locality,
+		COUNT(d.RestaurantID) AS [Số nhà hàng],
+		DENSE_RANK() OVER(PARTITION BY d.City 
+					 ORDER BY COUNT(d.RestaurantID)) AS [Xếp hạng]
+	FROM
+		dbo.Zomato_Dataset d
+	INNER JOIN 
+		dbo.Country_Name c
+	ON 
+		d.CountryCode = c.[Country Code]
+	WHERE 
+		c.Country = 'India'
+	GROUP BY 
+		c.Country, d.City, d.Locality
+)
+SELECT * FROM ct1 WHERE ct1.[Xếp hạng] = 1  AND city = 'Agra'
+GO
+
+
+-- Loại ẩm thực (cuisines) được áp dụng tại các nhà hàng trong 
+-- khu vực với số lượng nhà hàng xếp thứ x (x là số tự nhiên)
+
+WITH ct1 as
+(
+	SELECT
+		c.Country,
+		d.City,
+		d.Locality,
+		COUNT(d.RestaurantID) AS [Số nhà hàng],
+		DENSE_RANK() OVER(PARTITION BY d.City 
+					 ORDER BY COUNT(d.RestaurantID) DESC) AS [Xếp hạng]
+	FROM
+		dbo.Zomato_Dataset d
+	INNER JOIN 
+		dbo.Country_Name c
+	ON 
+		d.CountryCode = c.[Country Code]
+	WHERE 
+		c.Country = 'India'
+	GROUP BY 
+		c.Country, d.City, d.Locality
+),
+	ct2 AS
+(
+	SELECT	
+		d.Locality,
+		TRIM(N.Cuisines) AS [Cuisines]
+	FROM dbo.Zomato_Dataset d
+	CROSS APPLY 
+	(SELECT value AS [Cuisines] FROM STRING_SPLIT([Cuisines], '|')) N
+)
+SELECT 
+	ct1.Locality, 
+	ct2.Cuisines,
+	COUNT(ct2.Cuisines) AS [Số lượng nhà hàng áp dụng]
+FROM 
+	ct1 INNER JOIN ct2 ON ct2.Locality = ct1.Locality
+
+WHERE 
+	ct1.[Xếp hạng] = 1 AND ct1.city = 'Agra'
+GROUP BY 
+	ct1.Locality,
+	ct2.Cuisines
+GO
+
+-- Số lượng nhà hàng cung cấp lựa chọn đặt bàn (Has Table Booking) trong khu vực đông nhà hàng thứ x
+
+WITH ct1 as
+(
+	SELECT
+		c.Country,
+		d.City,
+		d.Locality,
+		COUNT(d.RestaurantID) AS [Số nhà hàng],
+		DENSE_RANK() OVER(PARTITION BY d.City 
+					 ORDER BY COUNT(d.RestaurantID)) AS [Xếp hạng theo số nhà hàng]
+		FROM 
+			dbo.Zomato_Dataset d
+	JOIN 
+		dbo.Country_Name c
+	ON 
+		d.CountryCode = c.[Country Code]
+	GROUP BY 
+		c.Country, d.City, d.Locality
+),
+	ct2 AS
+(
+	SELECT 
+		d.Locality,
+		COUNT(d.Has_Table_booking) [Số nhà hàng cho phép đặt bàn]
+	FROM 
+		dbo.Zomato_Dataset d
+	JOIN 
+		dbo.Country_Name c
+	ON 
+		d.CountryCode = c.[Country Code]
+	WHERE d.Has_Table_booking = 'Yes'
+	GROUP BY 
+		d.Locality
+)
+SELECT 
+	ct1.Country,
+	ct1.City, 
+	ct2.Locality, 
+	ct1.[Số nhà hàng],
+	ct1.[Xếp hạng theo số nhà hàng], 
+	ct2. [Số nhà hàng cho phép đặt bàn]
+FROM 
+	ct1 JOIN ct2 ON ct2.Locality = ct1.Locality  
+WHERE 
+	ct1.[Xếp hạng theo số nhà hàng] < 5  AND ct1.Country = 'India'
+
+-- Nghiên cứu xem với trường hợp null thì thay thế = 0 như thế nào
+
+
+
+SELECT * FROM dbo.Zomato_Dataset WHERE Locality = 'Connaught Place'
+
+
+
+/*
+Ảnh hưởng của điểm rating đến khu vực có số lượng nhà hàng lớn nhất 
+trong điều kiện có cung cấp và không cung cấp lựa chọn đặt bàn
+*/ 
+
+-- Lưu ý viết stored Procude hay function
+SELECT 
+	Locality,
+	N'Có' [Cung cấp lựa chọn đặt bàn],
+	COUNT([Has_Table_booking]) [Số nhà hàng], 
+	ROUND(AVG([Rating]),2) [Raiting trung bình]
+FROM 
+	dbo.Zomato_Dataset
+WHERE	
+	[Has_Table_booking] = 'Yes'
+	AND [City] = 'New Delhi'
+GROUP BY Locality
+UNION
+SELECT 
+	Locality,
+	N'Không' [Cung cấp lựa chọn đặt bàn],
+	COUNT([Has_Table_booking]) [Số nhà hàng], 
+	ROUND(AVG([Rating]),2) [Raiting trung bình]
+FROM 
+	dbo.Zomato_Dataset
+WHERE 
+	[Has_Table_booking] = 'No'
+	AND City = 'New Delhi' 
+GROUP BY Locality
+ORDER BY Locality 
+
+
+-- Điểm raiting trung bình của từng khu vực
+SELECT CountryCode ,City, Locality,
+COUNT([RestaurantID]) TOTAL_REST ,ROUND(AVG(CAST([Rating] AS DECIMAL)),2) AVG_RATING
+FROM dbo.Zomato_Dataset
+GROUP BY CountryCode, city, Locality
+ORDER BY TOTAL_REST DESC
+
+
+-- Lựa chọn nhà hàng phù hợp cho người Ấn Độ với mức giá cho 2 người thích hợp và có món ăn Ấn Độ (cuisines Indian)
---
+++ Exploration Data.sql
@@ -0,0 +1,287 @@
+﻿CREATE DATABASE Zomato
+USE Zomato
+GO
+
+--ALTER TABLE dbo.Zomato_Dataset
+--ALTER COLUMN Has_Table_booking BIT
+
+--EXPLORATION DATA
+SELECT * FROM INFORMATION_SCHEMA.COLUMNS
+
+-- Kiểm tra datatype của từng bảng
+SELECT 
+	DISTINCT (TABLE_CATALOG),
+	TABLE_NAME
+FROM 
+	INFORMATION_SCHEMA.COLUMNS
+
+SELECT 
+	COLUMN_NAME,
+	DATA_TYPE
+FROM 
+	INFORMATION_SCHEMA.COLUMNS
+WHERE 
+	TABLE_NAME = 'Zomato_Dataset'
+
+-- Xem dữ liệu của từng bảng
+
+SELECT TOP 10 * FROM dbo.Zomato_Dataset
+
+SELECT * FROM dbo.Country_Name
+
+-- Kiểm tra dữ liệu trùng trong database
+SELECT 
+	RestaurantID,
+	COUNT(RestaurantID) AS [Số lượng]
+FROM 
+	dbo.Zomato_Dataset
+GROUP BY 
+	RestaurantID
+ORDER BY 
+	[Số lượng] DESC
+GO
+-- NHẬN XÉT: Không có hiện tượng trùng lặp dữ liệu trong database
+
+
+-- KHÁM PHÁ DỮ LIỆU CỦA TỪNG CỘT TRONG CÁC BẢNG
+
+/* 
+	Tên nhà hàng (restauranname) và tên thành phố (city) có dấu hiệu lỗi font chữ
+	Tuy nhiên, tên nhà hàng không quá quan trọng và khó sửa nên giữ nguyên
+	Vì vậy, trong phạm vi file này, tác giả chỉ sửa lỗi font chữ của cột City
+*/
+-- Xác định tên thành phố bị lỗi
+SELECT DISTINCT RestaurantName FROM dbo.Zomato_Dataset 
+WHERE RestaurantName LIKE '%?%'
+
+
+SELECT 
+	REPLACE(City, '?', 'i')
+FROM 
+	dbo.Zomato_Dataset 
+WHERE 
+	CITY LIKE '%?%'
+
+-- Sửa lại tên thành phố bị lỗi
+UPDATE 
+	dbo.Zomato_Dataset 
+SET City = REPLACE(City, '?', 'i')
+			FROM 
+				dbo.Zomato_Dataset 
+			WHERE 
+				CITY LIKE '%?%'
+GO
+
+-- Xác định số nhà hàng tại mỗi thành phố ở mỗi quốc gia
+SELECT 
+	c.[Country Code],
+	c.Country AS[Country Name],
+	d.City,
+	COUNT(d.RestaurantID) AS [Số lượng nhà hàng]
+FROM 
+	dbo.Zomato_Dataset d
+INNER JOIN
+	dbo.Country_Name c
+ON d.CountryCode = c.[Country Code]
+GROUP BY 
+	c.[Country Code],
+	c.Country,
+	d.City
+ORDER BY 
+	[Số lượng nhà hàng] DESC
+GO
+
+
+--Cột Cuisines (ẩm thực)
+--Thông tin các nhà hàng có cột Cuisines null hoặc để trống
+SELECT
+	*
+FROM 
+	dbo.Zomato_Dataset
+WHERE 
+	Cuisines IS NULL OR Cuisines = ''
+
+-- Số lượng các nhà hàng có cột cuisines null hoặc để trống
+SELECT 
+	COUNT(RestaurantID) AS [Số lượng]
+FROM 
+	dbo.Zomato_Dataset
+WHERE 
+	Cuisines IS NULL OR Cuisines = ''
+GROUP BY 
+	Cuisines
+GO
+-- Nhận xét: Trong database hiện tại có 9 nhà hàng có cột cuisines null hoặc để trống
+
+
+-- Số lượng phong cách ẩm thực mà các nhà hàng áp dụng
+SELECT 
+	Cuisines, 
+	COUNT(Cuisines) AS [Số lượng]
+FROM 
+	dbo.Zomato_Dataset
+GROUP BY 
+	Cuisines
+ORDER BY 
+	[Số lượng] DESC
+GO
+--Nhận xét: Ẩm thực chủ đạo Bắc Ấn Độ (North Indian) xuất hiện phổ biến nhất tại các nhà hàng
+
+
+-- Cột Currency (tiền tệ)
+SELECT 
+	Currency,
+	COUNT(Currency) AS [Số lượng]
+FROM 
+	dbo.Zomato_Dataset
+GROUP BY 
+	Currency
+ORDER BY 
+	[Số lượng] DESC
+GO
+--Nhận xét: Tiền tệ được sử dụng phổ biến nhất là đồng Rupe của Ấn Độ và đồng Dollar
+
+
+-- Các cột có dữ liệu dạng Yes/No
+
+--Cột Has_Table_booking (có bàn đặt trước)
+SELECT 
+	Has_Table_booking,
+	COUNT(Has_Table_booking) AS [Số lượng]
+FROM 
+	dbo.Zomato_Dataset
+GROUP BY 
+	Has_Table_booking
+GO
+-- Nhận xét: Đa số các nhà hàng chưa có hệ thống đặt bàn trước
+
+-- Cột Has_Online_delivery (có dịch vụ giao đồ ăn online)
+SELECT 
+	Has_Online_delivery,
+	COUNT(Has_Online_delivery) AS [Số lượng]
+FROM
+	dbo.Zomato_Dataset
+GROUP BY 
+	Has_Online_delivery
+GO
+-- Nhận xét: Đa số nhà hàng chưa có dịch vụ giao đồ ăn online
+
+-- Cột Is_delivering_now
+SELECT 
+	Is_delivering_now,
+	COUNT(Is_delivering_now) AS [Số lượng]
+FROM
+	dbo.Zomato_Dataset
+GROUP BY 
+	Is_delivering_now
+GO
+
+-- Cột Switch_to_order_menu
+SELECT 
+	Switch_to_order_menu,
+	COUNT(Switch_to_order_menu) AS [Số lượng]
+FROM
+	dbo.Zomato_Dataset
+GROUP BY 
+	Switch_to_order_menu
+GO
+--Nhận xét: Chưa có nhà hàng nào có thực đơn online
+
+
+-- Cột Price_range
+SELECT 
+	Price_range,
+	COUNT(Price_range) AS [Số lượng]
+FROM 
+	dbo.Zomato_Dataset
+GROUP BY 
+	Price_range
+ORDER BY [Số lượng] DESC
+GO
+/*
+1 = Inexpensive, usually $10 and under
+2 = Moderately expensive, usually between $10-$25
+3 = Expensive, usually between $25-$45
+4 = Very Expensive, usually $50 and up
+*/
+-- Nhận xét: khoảng giá chênh lệch (price range) chủ yếu của các nhà hàng là 1 (không đắt)
+
+
+-- Cột Votes
+-- Kiểm tra các giá trị nhỏ nhất, giá trị trung bình, giá trị lớn nhất của cột
+SELECT
+	MIN(Votes) AS [Điểm vote thấp nhất],
+	AVG(Votes) AS [Điểm vote trung bình],
+	MAX(votes) AS [Điểm vote cao nhất]
+FROM 
+	dbo.Zomato_Dataset
+GO
+
+-- Kiểm tra lượng phân bổ điểm vote của các nhà hàng
+SELECT 
+	Votes, 
+	COUNT(Votes) AS [Số lượng]
+FROM 
+	dbo.Zomato_Dataset
+GROUP BY 
+	Votes
+ORDER BY 
+	[Số lượng] DESC
+-- Nhận xét: vote 0 chiếm tỉ lệ lớn 
+GO
+
+-- Cột Cost
+SELECT 
+	Currency,
+	MIN(Average_Cost_for_two) AS [Giá nhỏ nhất],
+	AVG(Average_Cost_for_two) AS [Giá trung bình],
+	MAX(Average_Cost_for_two) AS [Giá lớn nhất]
+FROM 
+	dbo.Zomato_Dataset 
+GROUP BY 
+	Currency
+GO
+
+
+-- Cột Raiting
+-- Kiểm tra các giá trị nhỏ nhất, giá trị trung bình, giá trị lớn nhất của cột
+SELECT 
+	MIN(Rating),
+	ROUND(MAX(Rating),1),
+	ROUND(AVG(Rating),1)
+FROM
+	dbo.Zomato_Dataset
+GO
+
+-- Kiểm tra lượng phân bổ raiting (đã làm tròn 2 chữ số thập phân) của các nhà hàng
+SELECT
+	ROUND(Rating,2) [Raiting làm tròn 2 chữ số thập phân],
+	COUNT(ROUND(Rating,2)) AS [số lượng]
+FROM 
+	dbo.Zomato_Dataset
+GROUP BY 
+	ROUND(Rating,2)
+ORDER BY 
+	[số lượng] DESC
+
+/*
+Thêm một cột "Rate_Category" thể hiện:
+	- Raiting trong nửa khoảng 1 - 2,5: Poor
+	- Raiting trong nửa khoảng 2,5 - 3,5: Good
+	- Raiting trong nửa khoảng 3,5 - 4,5: Great
+	- Raiting từ 4,5 trở lên: Excellent
+
+*/ 
+ALTER TABLE dbo.Zomato_Dataset ADD Rate_Category VARCHAR(20)
+UPDATE 
+	dbo.Zomato_Dataset
+SET [Rate_Category] =
+	(CASE 
+		WHEN Rating >= 1 AND Rating < 2.5 THEN 'Poor'
+		WHEN Rating >= 2.5 AND Rating < 3.5 THEN 'Good'
+		WHEN Rating >= 3.5 AND Rating < 4.5 THEN 'Great'
+		ELSE 'Excellent'
+	END)
+GO
+
+SELECT TOP 10 * FROM dbo.Zomato_Dataset
